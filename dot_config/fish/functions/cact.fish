ceedab087f733e7ce5740fc4547b8bdc05cdfdb4
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2026-01-11 09:20:12 +0100

    ref: remove conda-related functions and commands from Fish configuration

--- dot_config/fish/functions/cact.fish
+++
@@ -1,7 +0,0 @@
-function cact --description "Interactive conda activate"
-    set -l env (conda env list | tail -n +3 | awk '{print $1}' | grep -v "^$" | fzf --height=40% --prompt="Activate env: ")
-
-    if test -n "$env"
-        conda activate $env
-    end
-end
